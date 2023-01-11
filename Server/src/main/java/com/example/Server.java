0ee044eeea28ddcbec6770858d7151623b6af3a6
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-12 00:37:39 +0300

    chat socket created

---
+++ Server/src/main/java/com/example/Server.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.net.*;
+import java.util.ArrayList;
+import java.io.*;  
+public class Server{  
+
+    ArrayList<String> incoming = new ArrayList<>();
+
+   public ArrayList<String> Comm() throws IOException{
+        ServerSocket ss=new ServerSocket(3333);  
+        Socket s=ss.accept();  
+        DataInputStream din=new DataInputStream(s.getInputStream());  
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+        
+        String str="",str2="";  
+        while(!str.equals("stop")){  
+            str=din.readUTF();  
+            System.out.println("client says: "+str);  
+            str2=br.readLine();  
+            dout.writeUTF(str2);  
+            dout.flush();  
+        }  
+        din.close();  
+        s.close();  
+        ss.close();
+        return n;  
+            }
+public static void main(String args[])throws Exception{  
+      Server server = new Server();
+      server.Comm();
+}}  
